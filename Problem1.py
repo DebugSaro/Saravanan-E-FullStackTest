ebb41497fda366135a19e2469bee59f7e9b15a43
Saravanan <125845901+DebugSaro@users.noreply.github.com>
2025-10-27 10:48:37 +0530

    Create a small calculator which performs operations such as Addition, Subtraction, Multiplication and Division using class.
    Implemented a Calculator class that performs basic arithmetic operations.

--- Problem1.py
+++ Problem1.py
@@ -1,1 +1,23 @@
+/*Create a small calculator which performs operations such as Addition, Subtraction, Multiplication and Division using class.
+  Calculator inputs :> ‘a’, ‘b’ and ‘type of operation’
+  Datatype :> ‘a’ = double, ‘b’ = double, ‘type of operation’ = string*/
 
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def calculate(self, operation):
+        if operation == 'add':
+            return self.a + self.b
+        elif operation == 'subtract':
+            return self.a - self.b
+        elif operation == 'multiply':
+            return self.a * self.b
+        elif operation == 'divide':
+            return self.a / self.b
+        else:
+            return "Invalid operation"
+          
+calc = Calculator(10, 2)
+print(calc.calculate('add'))
